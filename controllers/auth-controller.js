4b4538a4f37b91924bc0cbe285e5c9cd4be50dc9
Marouane Bachar <marstrone@gmail.com>
2025-08-12 23:12:47 +0200

    validation

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,7 +1,12 @@
 const bcryptjs = require("bcryptjs");
 const User = require("../models/user-model");
+const {validationResult} = require("express-validator");
 
 const register = async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
     try {
         const { username, email, password } = req.body;
 
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,10 +1,15 @@
 const {register} = require("../controllers/auth-controller");
 const express = require("express");
 const authRoutes = express.Router();
+const {check} = require("express-validator");
 
+const registerValidationRules=[
+    check("username").notEmpty().withMessage("username is required"),
+    check("email").isEmail().withMessage("invalid email"),
+    check("password").isLength({min:6}).withMessage("Password must be at least 6 characters long")
+]
 
-
-authRoutes.post("/register" ,register);
+authRoutes.post("/register",registerValidationRules ,register);
 
 
 module.exports = authRoutes ;
