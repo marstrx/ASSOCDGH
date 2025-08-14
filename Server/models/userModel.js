80b591eeaf2c18b46c7e540a706daf2269410898
Marouane Bachar <marstrone@gmail.com>
2025-08-14 13:15:41 +0200

    simple schema

---
+++ Server/models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+
+
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        // select: false 
+    },
+    phone: {
+        type: String,
+        trim: true
+    },
+    totalAmount: {
+        type: Number,
+        default: 0
+    },
+    verifyOtp: {
+        type: String,
+        default: ""
+    },
+    verifyOtpExpireAt: {
+        type: Number
+    },
+    isAccountVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetOtp: {
+        type: String,
+        default: ""
+    },
+    resetOtpExpireAt: {
+        type: Number
+    }
+}, {timestamps: true });
+
+
+
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
